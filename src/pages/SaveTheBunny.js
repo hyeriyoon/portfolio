34dad1deb4bc16917a25f68985db7439e33ccb3d
hyeriyoon <109633824+hyeriyoon@users.noreply.github.com>
2023-02-21 16:33:21 -0800

    add save the bunny description

--- src/pages/SaveTheBunny.js
+++ src/pages/SaveTheBunny.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import SingleWork from '../components/SingleWork'
-// import tweeling from '../images/desktop-tweeling.png';
+import savethebunny from '../images/desktop-savethebunny.png'
 
 export default function SaveTheBunny() {
   return (
     <SingleWork 
       title="Save the Bunny"
-      image={tweeling}
+      image={savethebunny}
       github='https://github.com/hyeriyoon/savethebunny.git'
       project='https://www.haileeyoon.com/savethebunny'
       subtitle='Javascript Game Project'
       tools="HTML, CSS, JavaScript, Adobe XD, Adobe Photoshop"
-      // overview=''
+      overview='Save the Bunny is JavaScript game built using HTML, CSS and JavaScript. User can select 3 level and reset and replay the game.'
     />
   )
 }
